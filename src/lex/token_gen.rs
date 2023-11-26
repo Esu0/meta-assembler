28a49986b8aeeba5c25706e2bcff5e0bb4318617
Shintaro Shimada <87416336+Esu0@users.noreply.github.com>
2023-11-26 14:24:56 +0900

    CharGeneratorが動く様に修正 (#1)

--- src/lex/token_gen.rs
+++ src/lex/token_gen.rs
@@ -1,5 +1,7 @@
 // const OPERATION_MAP: &'static [(&'static str, Token)] = &[("+", Token::Add)];
 
+use std::io;
+
 pub enum Token {
     // token kinds
     Add,
@@ -12,13 +14,13 @@ pub struct TokenGenerator<C> {
     // additional field
 }
 
-impl<C: Iterator<Item = char>> TokenGenerator<C> {
+impl<C: Iterator<Item = io::Result<char>>> TokenGenerator<C> {
     pub fn new(chars: C) -> Self {
         Self { chars }
     }
 }
 
-impl<C: Iterator<Item = char>> Iterator for TokenGenerator<C> {
+impl<C: Iterator<Item = io::Result<char>>> Iterator for TokenGenerator<C> {
     type Item = Token;
     fn next(&mut self) -> Option<Self::Item> {
         unimplemented!()
