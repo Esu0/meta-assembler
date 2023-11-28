fcdfd73a2202a6587b62f14da3704f52a8c42607
Esu0 <jshintaro1018@gmail.com>
2023-11-28 14:12:03 +0900

    Revert "TokenGeneratorのイテレータのItemの型をResultでラップした (#3)"
    This reverts commit fd33e9bc6e5098e16203557ffadee26bd6b04565.

--- src/lex.rs
+++ src/lex.rs
@@ -7,34 +7,11 @@ pub mod token_gen;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-enum _Error {
+enum Error {
     /// オーバーフローを含む
     #[error("line {line}: Token \"{token}\" cannot parse to integer.")]
-    ParseIntError { line: usize, token: String },
-}
-
-#[derive(Debug, Error)]
-#[error(transparent)]
-pub struct Error {
-    #[from]
-    inner: _Error,
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn error_test() {
-        let e = Error {
-            inner: _Error::ParseIntError {
-                line: 1,
-                token: "100a".to_owned(),
-            },
-        };
-        assert_eq!(
-            e.to_string(),
-            "line 1: Token \"100a\" cannot parse to integer."
-        );
-    }
+    ParseIntError {
+        line: usize,
+        token: String,
+    },
 }
