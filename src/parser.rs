1fa601b83c96edc4704e257bc887f60f160fd5a6
Esu0 <jshintaro1018@gmail.com>
2023-11-28 16:11:48 +0900

    メタアセンブラのパーサ

--- src/parser.rs
+++ src/parser.rs
@@ -3,4 +3,68 @@
 pub mod assembly;
 pub mod cgr;
 
+use std::io;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ErrorKind {
+    #[error("予期せぬトークン{found}が検出されました。トークン{expected}が予測されます。")]
+    UnexpectedToken {expected: String, found: String},
+    #[error("ファイルの終端に到達しました。")]
+    UnexpectedEof,
+}
+
+#[derive(Error, Debug)]
+#[error("line {line}({column}): {kind}")]
+struct ErrorSimple {
+    kind: ErrorKind,
+    line: usize,
+    column: usize,
+}
+
+#[derive(Error, Debug)]
+enum ErrorInner {
+    #[error(transparent)]
+    Simple(#[from] ErrorSimple),
+    #[error(transparent)]
+    Custom(Box<dyn std::error::Error + Sync + Send>),
+}
+
+#[derive(Error, Debug)]
+#[error(transparent)]
+pub struct Error {
+    inner: ErrorInner,
+}
+
+impl Error {
+    pub fn kind(&self) -> Option<&ErrorKind> {
+        match self.inner {
+            ErrorInner::Simple(ref e) => Some(&e.kind),
+            ErrorInner::Custom(_) => None,
+        }
+    }
+
+    pub fn new(kind: ErrorKind, position: (usize, usize)) -> Self {
+        Self {
+            inner: ErrorInner::Simple(ErrorSimple { kind, line: position.0, column: position.1 })
+        }
+    }
+}
+
+impl From<crate::lex::Error> for Error {
+    fn from(value: crate::lex::Error) -> Self {
+        Self {
+            inner: ErrorInner::Custom(Box::new(value)),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(value: io::Error) -> Self {
+        Self {
+            inner: ErrorInner::Custom(Box::new(value)),
+        }
+    }
+}
 // exports
--- src/parser/cgr.rs
+++ src/parser/cgr.rs
@@ -1,1 +1,59 @@
 //! コード生成ルールファイルの構文解析
+
+use std::io;
+
+use crate::lex::token_gen::{TokenGenerator, Token};
+
+use super::{Error, ErrorKind};
+
+pub enum Node {
+    Integer(u64),
+    Identifier(String),
+    Comma,
+    Semicolon,
+}
+
+impl From<Token> for Node {
+    fn from(value: Token) -> Self {
+        match value {
+            Token::Integer(n) => Self::Integer(n),
+            Token::Opr(ref s) => {
+                match s.as_str() {
+                    "," => Self::Comma,
+                    ";" => Self::Semicolon,
+                    _ => unimplemented!()
+                }
+            }
+            Token::Word(w) => {
+                Self::Identifier(w)
+            }
+        }
+    }
+}
+
+pub struct Parser<C> {
+    token_generator: TokenGenerator<C>,
+}
+
+impl<C: Iterator<Item = io::Result<char>>> Parser<C> {
+    pub fn new(token_generator: TokenGenerator<C>) -> Self {
+        Self { token_generator }
+    }
+
+    fn next_node(&mut self) -> Option<Result<Node, Error>> {
+        self.token_generator.next().map(|x| x.map(Node::from).map_err(super::Error::from))
+    }
+
+    fn next_number(&mut self) -> Result<u64, Error> {
+        if let Some(token) = self.token_generator.next() {
+            match token? {
+                Token::Integer(n) => Ok(n),
+                token => 
+                Err(Error::new(ErrorKind::UnexpectedToken { expected: "Integer".to_owned(), found: "".to_owned() }, self.token_generator.inner().position())),
+            }
+            
+        } else {
+            Err(io::Error::from(io::ErrorKind::UnexpectedEof).into())
+        }
+    }
+}
